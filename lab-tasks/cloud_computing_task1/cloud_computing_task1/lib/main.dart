4fe90150d255f8cb8a26c0d07dfb15186b49e390
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-11-25 20:41:15 +0200

    Update main.dart

--- lab-tasks/cloud_computing_task1/cloud_computing_task1/lib/main.dart
+++ lab-tasks/cloud_computing_task1/cloud_computing_task1/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_computing_task1/screens/add_group_screen.dart';
-import 'package:cloud_computing_task1/screens/list_groups_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
